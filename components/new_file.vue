507c7bee7efe4f1c9a10cb45bdff448303ace9a5
lzj <1104792149@qq.com>
2019-03-10 16:55:35 +0800

    模板

---
+++ components/new_file.vue
@@ -0,0 +1,32 @@
+<template>
+	<view id="app">
+	</view>
+</template>
+
+<script>
+    //import item1 from '@/components/common/item1'//组件
+	export default {
+		data() {
+			return {
+				msg: ''
+			}
+		},
+		onLoad() {
+			
+		},
+		//事件
+		methods: {},
+		//模板
+		components: {},
+         //过滤器
+		computed: {
+
+		},
+		
+		           
+	}
+</script>
+
+<style>
+
+</style>
